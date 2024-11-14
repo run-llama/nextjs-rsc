121ed260bc9143eaaa1e6181df271954f472b8d8
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-11-14 12:16:59 +0700

    feat: migrate to RSC

---
+++ app/components/ui/chat/custom/annotations.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useChatUI } from "@llamaindex/chat-ui";
+import { ChatEvents, SuggestedQuestions } from "@llamaindex/chat-ui/widgets";
+
+import {
+  DocumentFileData,
+  getAnnotationData,
+  ImageData,
+  MessageAnnotation,
+  MessageAnnotationType,
+} from "@llamaindex/chat-ui";
+import { ChatFiles, ChatImage } from "@llamaindex/chat-ui/widgets";
+import { JSONValue } from "ai";
+import { Markdown } from "./markdown";
+
+export function CustomSuggestedQuestions({
+  questions,
+}: {
+  questions: string[];
+}) {
+  const { append } = useChatUI();
+  return <SuggestedQuestions questions={questions} append={append} />;
+}
+
+export function CustomChatEvents({ events }: { events: { title: string }[] }) {
+  const { isLoading } = useChatUI();
+  return <ChatEvents data={events} showLoading={isLoading} />;
+}
+
+export function UserMessageDisplay({
+  content,
+  annotations,
+}: {
+  content: string;
+  annotations?: JSONValue[];
+}) {
+  const messageAnnotations = annotations as MessageAnnotation[];
+  const imageData = getImageData(messageAnnotations);
+  const documentFileData = getDocumentFileData(messageAnnotations);
+
+  return (
+    <div className="space-y-2">
+      <Markdown content={content} />
+      {imageData && <ChatImage data={imageData} />}
+      {documentFileData && <ChatFiles data={documentFileData} />}
+    </div>
+  );
+}
+
+function getImageData(annotations?: MessageAnnotation[]) {
+  return annotations && annotations.length > 0
+    ? getAnnotationData<ImageData>(annotations, MessageAnnotationType.IMAGE)[0]
+    : undefined;
+}
+
+function getDocumentFileData(annotations?: MessageAnnotation[]) {
+  return annotations && annotations.length > 0
+    ? getAnnotationData<DocumentFileData>(
+        annotations,
+        MessageAnnotationType.DOCUMENT_FILE,
+      )[0]
+    : undefined;
+}
---
+++ app/components/use-chat-rsc.tsx
@@ -0,0 +1,51 @@
+import { useActions } from "ai/rsc";
+
+import { generateId, Message } from "ai";
+import { useUIState } from "ai/rsc";
+import { useState } from "react";
+import { AIProvider } from "../rsc/ai";
+import { UserMessageDisplay } from "./ui/chat/custom/annotations";
+
+export function useChatRSC() {
+  const [input, setInput] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [messages, setMessages] = useUIState<AIProvider>();
+  const { chat } = useActions<AIProvider>();
+
+  const append = async (message: Omit<Message, "id">, data?: any) => {
+    const newMsg: Message = { ...message, id: generateId() };
+
+    setIsLoading(true);
+    try {
+      setMessages((prev) => [
+        ...prev,
+        {
+          ...newMsg,
+          display: (
+            <UserMessageDisplay
+              content={message.content}
+              annotations={message.annotations}
+            />
+          ),
+        },
+      ]);
+      const assistantMsg = await chat(newMsg, data);
+      setMessages((prev) => [...prev, assistantMsg]);
+    } catch (error) {
+      console.error(error);
+    }
+    setIsLoading(false);
+    setInput("");
+
+    return message.content;
+  };
+
+  return {
+    input,
+    setInput,
+    isLoading,
+    messages,
+    setMessages,
+    append,
+  };
+}
